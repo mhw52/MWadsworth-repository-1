7ed87a9702c64e81b2ce67644353a6445f64cc4f
mhw52 <lind.mariah@gmail.com>
2015-09-09 09:57:22 -0600

    Variables and Functions

---
+++ New Unity Project/Assets/variablescript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class variablescript : MonoBehaviour 
+{
+	int myInt = 5;
+
+	void Start(){
+		myInt = MultiplyByTwo (myInt);
+		Debug.Log (myInt);
+	
+	}
+	int MultiplyByTwo(int Number){
+
+		int ret;
+		ret = Number * 2;
+		return ret;
+	}
+}
