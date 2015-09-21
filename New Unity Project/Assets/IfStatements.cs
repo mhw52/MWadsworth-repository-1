51f23f5e238d44b9d66a3255445a1bb8c2c4bd96
mhw52 <lind.mariah@gmail.com>
2015-09-21 10:17:30 -0600

    If Statements

---
+++ New Unity Project/Assets/IfStatements.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour
+{
+	float coffeeTemperature = 85.0f;
+	float hotLimitTemperature = 70.0f;
+	float coldLimitTemperature = 40.0f;
+	
+	
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+		
+		coffeeTemperature -= Time.deltaTime * 5f;
+	}
+
+
+	void TemperatureTest (){
+		if (coffeeTemperature > hotLimitTemperature) {
+			print ("The Coffee is too Hot.");
+		} else if (coffeeTemperature < coldLimitTemperature) {
+			print ("The Coffee is too Cold.");
+		} else {
+			print ("The Coffee is just right.");
+		}
+	}
+
+}
+
