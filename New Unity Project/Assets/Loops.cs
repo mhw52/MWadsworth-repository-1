4ecfc2cdc7fb41723c9dbac4d4c0489c8e1809bc
mhw52 <lind.mariah@gmail.com>
2015-09-23 09:56:00 -0600

    Loops project

---
+++ New Unity Project/Assets/Loops.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour
+{
+
+    int cupsinthesink = 4;
+
+     
+      void Start()
+      {
+          while (cupsinthesink > 0)
+          {
+              Debug.Log("I've washed a cup!");
+              cupsinthesink--;
+          }
+      }
+
+
+ /* 
+
+             void Start()
+                  {
+                      do
+                      {
+                          Debug.Log("I've washed a cup!");
+                          cupsinthesink--;
+                      } while (cupsinthesink > 0);
+
+                  }
+
+
+
+
+
+                  void Start()
+                  {
+                      for (int i=0; i<cupsinthesink; i++)
+                      {
+                          Debug.Log("I've washed a cup!");
+                      }
+                  }
+
+
+
+
+
+
+
+
+
+                    void Start()
+                    {
+                        string[] strings = new string[3];
+
+                        strings[0] = "First String";
+                        strings[1] = "Second String";
+                        strings[2] = "Third string";
+
+                        foreach (string Str in strings)
+                        {
+                            print(Str);
+                        }
+
+                    }
+
+      */
+
+
+}
