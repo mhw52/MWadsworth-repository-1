d7ae3a8c59b90be98ac21923205a1d61c82d5089
mhw52 <lind.mariah@gmail.com>
2015-10-26 09:57:09 -0600

    Enumerations

---
+++ New Unity Project/Assets/EnumScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnumScript : MonoBehaviour {
+
+
+
+	enum Direction {North, East, South, West};
+
+
+	// Use this for initialization
+	void Start () {
+	
+		Direction myDirection;
+
+		myDirection = Direction.North;
+
+
+		
+	}
+
+	Direction ReverseDirection (Direction dir)
+	{
+		if (dir == Direction.North)
+			dir = Direction.South;
+		else if (dir == Direction.South)
+			dir = Direction.North;
+		else if (dir == Direction.East)
+			dir = Direction.West;
+		else if (dir == Direction.West)
+			dir = Direction.East;
+
+		
+
+			return dir;
+
+	}
+
+
+
+
+}
