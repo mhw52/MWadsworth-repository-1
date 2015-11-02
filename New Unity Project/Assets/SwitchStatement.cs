0a5b5a91d34d50c7c4f6a5754e6af293d894e1d1
mhw52 <lind.mariah@gmail.com>
2015-11-02 09:16:28 -0700

    Trick or Treat Switch Statements

---
+++ New Unity Project/Assets/SwitchStatement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchStatement : MonoBehaviour {
+
+	public int age = 5;
+
+
+	void Start()
+	{
+		Greet ();
+	}
+
+	void Greet()
+	{
+	
+		switch (age) {
+
+		case 5: 
+			print("reeses pieces!");
+			break;
+		case 4: 
+			print("m&m!");
+			break;
+		case 3: 
+			print("suckers");
+			break;
+		case 2: 
+			print("jelly beans!");
+			break;
+		case 1: 
+			print("chocolate");
+			break;
+
+		default:
+			print ("candy");
+			break;
+
+
+
+		}
+
+
+	}
+	
+
+}
