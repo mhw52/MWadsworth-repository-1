a41f9f9ba4fdc56c694d339daa663c5cbdb16199
unknown <10625299@CS514-19.ad.uvu.edu>
2015-12-09 09:43:13 -0700

    Dog animation complete

--- New Unity Project/Assets/DogControl.cs
+++ New Unity Project/Assets/DogControl.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class DogControl : MonoBehaviour {
 
+	Animator anim;
+
 	// Use this for initialization
 	void Start () {
-	
+		GameManager.OnDuckDeath += PlayDuck;
+		GameManager.OnDuckFlyAway += PlayLaugh;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	//void Update () {
 	
+	//}
+
+	public void SpawnDuck()
+	{
+		GameManager.OnSpawnDuck ();
+	}
+	public void PlayLaugh()
+	{
+		anim.Play ("DogLaugh");
+	}
+
+	public void PlayDuck()
+	{
+		anim.Play ("DogDuck");
 	}
 }
